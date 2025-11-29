be82e8b8e98009ceee3ba832c783dea97482aebe
Junbo Zheng <3273070@qq.com>
2025-11-29 22:35:44 +0800

    add more test case for log.py
    Signed-off-by: Junbo Zheng <3273070@qq.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+set -e
+
 flake8 . --count --show-source --statistics
 
 black --check --diff .
+
+./miwear/log.py -s ./test --filename 123
+
+./miwear/targz.py --path test
+
+./miwear/gz.py --path test
+
+./miwear/unzip.py --path test
+
+md5sum -c test/123.md5
+
+./miwear/log.py -s test -f ./test/456（1）.tar.gz
